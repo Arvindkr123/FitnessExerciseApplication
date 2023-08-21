f2e8822c50256b33b3f434997c29dad5f32855a0
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-08-21 19:32:53 +0530

    done navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer.jsx";
 const App = () => {
   return (
-    <Box>
+    <Box width={"400px"} sx={{ width: { xl: "1488px" } }} m={"auto"}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-  return (
-    <div>
-      Footer is a React component
-    </div>
-  )
-}
+  return <div>Footer</div>;
+};
 
 export default Footer;
