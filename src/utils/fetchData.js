8859772fe07523a36398b5c210242d16e5438d7e
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-08-21 21:28:58 +0530

    ADDED

---
+++ src/utils/fetchData.js
@@ -0,0 +1,14 @@
+export const ExerciseOptions = {
+  method: "GET",
+  url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+  headers: {
+    "X-RapidAPI-Key": import.meta.env.REACT_VITE_APP_API_KEY,
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+};
